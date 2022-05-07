f061fbd47fc64ad2ef205c1b45a550d05d8a719c
AmilsonAp <amilson.moreira@gmail.com>
2022-05-06 21:12:02 -0300

    v0

---
+++ Consumer/src/main/java/br/com/projAulaSpring/model/Client.java
@@ -0,0 +1,49 @@
+package br.com.projAulaSpring.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.io.Serializable;
+
+@Document
+public class Client implements Serializable {
+
+    @Id
+    private String _id;
+    private String Description;
+    private String Date;
+    private int NumberOfStudent;
+
+    public Client(){}
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public int getNumberOfStudent() {
+        return NumberOfStudent;
+    }
+
+    public void setNumberOfStudent(int numberOfStudent) {
+        NumberOfStudent = numberOfStudent;
+    }
+}
